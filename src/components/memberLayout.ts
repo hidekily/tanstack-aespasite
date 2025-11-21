d86add0f89b7f7008d56b6e2c695a32bfecdc964
Denilson Kuratomi <mhtaue@gmail.com>
2025-11-21 12:56:35 +0800

    entendendo interface e props

---
+++ src/components/memberLayout.ts
@@ -0,0 +1,13 @@
+import {k, w, g, n} from '../data/memberData'
+
+function Membro({nome, idade, imgPos}: membroProps){
+    return(
+        <>
+            {imgPos === 'left' ? () : ()}
+        </>
+    )
+}
+
+<Membro {...k}/>
+
+
